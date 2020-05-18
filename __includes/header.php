fdeb7df4a0cafb57d73c9864dcb92d8dab2a8ab4
Elvis Blanco <elvisblanco1993@gmail.com>
2020-05-17 20:12:32 -0400

    ...

--- __includes/header.php
+++ __includes/header.php
@@ -1,12 +1,14 @@
-<header class="contanier">
-  <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap pt-0 pb-0">
-    <a class="navbar-brand" href="/">
-      <img src="http://momo/app/assets/img/logo.svg" height="36">
-      Webleads Docs
-    </a>
-    <span>
-      <?php echo $username; ?>
-      <?php echo $auth_btn; ?>
-    </span>
+<header>
+  <nav class="navbar navbar-light navbar-expand-md sticky-top bg-white">
+    <div class="container">
+      <a class="navbar-brand" href="/">
+        <img src="/assets/img/logo.svg" height="36">
+        Webleads Docs
+      </a>
+      <span>
+        <?php echo $username; ?>
+        <?php echo $auth_btn; ?>
+      </span>
+    </div>
   </nav>
-  </header>
+</header>
--- server/auth.php
+++ server/auth.php
@@ -23,20 +23,22 @@ if(isset($_GET['logout'])) {
 }
 
 if(isset($_SESSION['username'])) {
-  $username = '<span class="text-light"><svg class="bi bi-people-circle" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-  <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 008 15a6.987 6.987 0 005.468-2.63z"/>
-  <path fill-rule="evenodd" d="M8 9a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"/>
-  <path fill-rule="evenodd" d="M8 1a7 7 0 100 14A7 7 0 008 1zM0 8a8 8 0 1116 0A8 8 0 010 8z" clip-rule="evenodd"/>
-</svg> ' . $_SESSION['username'] . '</span>';
-  $auth_btn = '<a class="ml-5 text-light" href="/?logout">Log Out</a>';
+  $username = '<span class="text-light">
+                <svg class="bi bi-people-circle" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 008 15a6.987 6.987 0 005.468-2.63z"/>
+                  <path fill-rule="evenodd" d="M8 9a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"/>
+                  <path fill-rule="evenodd" d="M8 1a7 7 0 100 14A7 7 0 008 1zM0 8a8 8 0 1116 0A8 8 0 010 8z" clip-rule="evenodd"/>
+                </svg> ' . $_SESSION['username'] . '</span>';
+
+  $auth_btn = '<a class="ml-5 text-dark" href="/?logout">Log Out</a>';
   $user_loggedin = true;
 }
 
 // Configuration
-$client_id = '0oac2v2ebIMFdrk604x6';
-$client_secret = 'f6jr9jd6RrLvY9olBq-gVwtgmcy2A3mIHHAdexXS';
-$redirect_uri = 'http://localhost:4000';
-$metadata_url = 'https://dev-772697.okta.com/oauth2/default/.well-known/oauth-authorization-server';
+$client_id = '0oacaxyc2EQuvI7xv4x6';
+$client_secret = 'GuBSPpUSKGdcJDat6acOiNLoH_ciIsDI6PvUiPFa';
+$redirect_uri = 'https://help.webleads.app';
+$metadata_url = 'https://dev-772697.okta.com/oauth2/auscaxyg6lnaitKCU4x6/.well-known/oauth-authorization-server';
 $metadata = http($metadata_url);
 
 
@@ -87,5 +89,5 @@ if(!isset($_SESSION['username'])) {
   ]);
   #$authorize_url = 'TODO';
 
-  $auth_btn = '<a class="text-light" href="'.$authorize_url.'">Log In</a>';
+  $auth_btn = '<a class="text-dark" href="'.$authorize_url.'">Log In</a>';
 }
